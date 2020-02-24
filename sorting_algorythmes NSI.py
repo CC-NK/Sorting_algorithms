73e913fbb24952f88bbd68fec825946b54889d42
CC-NSI <58935995+CC-NSI@users.noreply.github.com>
2020-02-24 18:42:35 +0100

    Update sorting_algorythmes NSI.py

--- sorting_algorythmes NSI.py
+++ sorting_algorythmes NSI.py
@@ -4,7 +4,11 @@ def belong (n,T):
         if n == T[ImportWarning]:
             return True
         i= i+1
-        retrun False
+        return False
+
+def test_belong():
+    t=[1,4,8,2,3,7,9,15,1]
+    return belong(9,t) and not belong(5,t)
 
 def my_sum(t):
     res=0
@@ -19,14 +23,21 @@ def greatest_in(t):
             res=element
     return res
 
-def remove(t,i):
+def remove (t,i):
     res=(0)*(len(t)-1)
     for i in range (len(t)):
         if j<i:
             res[j]=t[j]
         if j>i:
             res [j-1]=t[j]
-    return (res)
+    return res
+
+def index_of_the_smallest(t):
+    res=t[i]
+    for indice in range (i+1,j+1):
+        if t[indice]<res:
+              res=t[indice]
+    return res
 
 def my_selection_sort (t):
     res=[0]*len(t)
@@ -34,7 +45,7 @@ def my_selection_sort (t):
         j= index_of_the_smallest(t)
         res[i]=t[j]
         remove(t,j)
-    return (res)
+    return res
 
 def my_len(t):
     cpt=0
@@ -48,17 +59,23 @@ def map_double(t):
         res [i]=2*t[i]
     return res
 
-def smallestin (t,j):
+def smallest_in (t,j):
     res=t[i]
     for indice in range (i+1,j+1):
         if t[indice]<res:
               res=t[indice]
     return res
 
+def test_smallest_in ():
+    t=[7,48,12,666]
+    i=1
+    j=3
+    return (12== smallestin (t,i,j))
+
 def swap (t,i,j):
     swap=t[i]
     t[i]=t[j]
-    t[j]= #je n'ai pas la suite de cet algo
+    t[j]=t[i] #j'ai essayé par logique mais pas sûr
 
 def selection_sort_inplace (t):
     for index in range (len(t)):
